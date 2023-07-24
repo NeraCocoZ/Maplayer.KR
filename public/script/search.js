39395c59225985d6b9e852e93952bed4e90ea83c
NeraCocoZ <neracocoz@kakao.com>
2023-07-24 14:23:33 +0000

    2023-07-24

---
+++ public/script/search.js
@@ -0,0 +1,15 @@
+/**
+ * 파일 경로: /public/script
+ * 파일 이름: search.js
+ * 
+ * 파일 작성자: NeraCocoZ
+ * 작성자 메일: neracocoz@gmail.com
+ * 
+ * 파일 생성일: 2023-07-24, 월
+ * 
+ * 이 파일은 "코드 정리 및 최적화"가 완료된 파일입니다.
+ */
+
+$(document).ready(() => {
+	
+});
